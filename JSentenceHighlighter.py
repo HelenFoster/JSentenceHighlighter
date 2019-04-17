7b212a39f29a96e67b2d87a3c9921910472b291e
Helen Foster <HelenFoster@users.noreply.github.com>
2019-04-17 16:11:27 +0100

    Update init for Anki 2.1

--- JSentenceHighlighter.py
+++ JSentenceHighlighter.py
@@ -1,32 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2017  Helen Foster
+# Copyright (C) 2017,2019  Helen Foster
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 # JSentenceHighlighter is an Anki addon for a Japanese vocab+sentence deck.
 # It searches and marks the vocab word in the sentence.
+
 # This file belongs with the "jsentencehighlighter" folder:
 #  the README might be inside depending on how you received this!
 
-from aqt import mw
-from PyQt4.QtCore import SIGNAL
-from PyQt4.QtGui import QAction
-from anki.hooks import addHook
-
-def highlightSentences(nids=None):
-    import jsentencehighlighter.run as run
-    reload (run)
-    run.highlightSentences(nids)
-
-action = QAction("Highlight all sentences (JSH)", mw)
-mw.connect(action, SIGNAL("triggered()"), highlightSentences)
-mw.form.menuTools.addAction(action)
-
-def addToBrowserMenu(browser):
-    def highlightSelected():
-        highlightSentences(browser.selectedNotes())
-    action = QAction("Highlight selected sentences (JSH)", browser)
-    browser.connect(action, SIGNAL("triggered()"), highlightSelected)
-    browser.form.menuEdit.addSeparator()
-    browser.form.menuEdit.addAction(action)
-
-addHook("browser.setupMenus", addToBrowserMenu)
+import jsentencehighlighter
---
+++ jsentencehighlighter/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017,2019  Helen Foster
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+# JSentenceHighlighter is an Anki addon for a Japanese vocab+sentence deck.
+# It searches and marks the vocab word in the sentence.
+
+from aqt import mw
+from aqt.qt import QAction
+from anki.hooks import addHook
+
+def highlightSentences(nids):
+    from . import run
+    reload(run)
+    run.highlightSentences(nids)
+
+def highlightAllSentences():
+    #A no-arg version is needed, otherwise new-style signal passes False
+    highlightSentences(None)
+
+action = QAction("Highlight all sentences (JSH)", mw)
+action.triggered.connect(highlightAllSentences)
+mw.form.menuTools.addAction(action)
+
+def addToBrowserMenu(browser):
+    def highlightSelected():
+        highlightSentences(browser.selectedNotes())
+    action = QAction("Highlight selected sentences (JSH)", browser)
+    action.triggered.connect(highlightSelected)
+    browser.form.menuEdit.addSeparator()
+    browser.form.menuEdit.addAction(action)
+
+addHook("browser.setupMenus", addToBrowserMenu)
